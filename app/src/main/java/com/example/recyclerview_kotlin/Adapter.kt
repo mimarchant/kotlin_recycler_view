2a5d14e4e5cd6f8c1bf5381f99940e2299a97294
mimarchant <mmarchant@moveapps.cl>
2023-07-19 19:47:01 -0400

    se agrega funcion que cambia el fondo segun tipo

--- app/src/main/java/com/example/recyclerview_kotlin/Adapter.kt
+++ app/src/main/java/com/example/recyclerview_kotlin/Adapter.kt
@@ -1,21 +1,17 @@
 package com.example.recyclerview_kotlin
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.recyclerview_kotlin.databinding.ItemBinding
 
-/*
-[x] Implementar clase Adapter
-[x] Heredar RecuclerView
-[x] Creaciones de métodos: onCreateViewHolder, onBindViewHolder y getItemCount
-[x] Crear class ViewHolder
-[x] Heredad RecyclerView.ViewHolder a class ViewHolder
-[x] Agregar constructor
-[x] Obtener datos
 
- */
 class Adapter: RecyclerView.Adapter<Adapter.ViewHolder>() {
 
     var pokemones = mutableListOf<Pokemon>()
@@ -25,8 +21,8 @@ class Adapter: RecyclerView.Adapter<Adapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: Adapter.ViewHolder, position: Int) {
-        var item = pokemones[position]
-        holder.bind(item)
+        val pokemon = pokemones[position]
+        holder.bind(pokemon)
     }
 
     override fun getItemCount(): Int {
@@ -42,7 +38,35 @@ class Adapter: RecyclerView.Adapter<Adapter.ViewHolder>() {
         fun bind(pokemon: Pokemon) {
             binding.textViewNombre.text = pokemon.nombre
             binding.textViewElemento.text = pokemon.tipo
-
+            val color = Color.parseColor(getTypeColor(pokemon.tipo))
+            binding.cardView.setCardBackgroundColor(color)
+            Glide.with(itemView.context)
+                .load(pokemon.imagen)
+                .into(binding.imageViewPoke)
         }
+    }
+}
+
+fun getTypeColor(type: String): String {
+    return when(type.toLowerCase()) {
+        "fire" -> "#FF4500"
+        "water" -> "#0000FF"
+        "grass" -> "#00FF00"
+        "poison" -> "#8A2BE2"
+        "normal" -> "#D3D3D3"
+        "rock" -> "#8B4513"
+        "electric" -> "#FFFF00"
+        "fairy" -> "#FFB6C1"
+        "fighting" -> "#B8860B"
+        "bug" -> "#006400"
+        "ghost" -> "#800080"
+        "grass - poison" -> "#9ACD32"
+        "fire - flying" -> "#FF8C00"
+        "bug - flying" -> "#9ACD32"
+        "bug - poison" -> "#9ACD32"
+
+
+        // add more types as needed
+        else -> "#FAF0E6" // a default color if no match found
     }
 }
